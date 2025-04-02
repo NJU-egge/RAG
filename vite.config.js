7ee8e08394bd1577b81d6af4dc1aa3c227488edf
孙 浩翔 <221900209@smail.nju.edu.cn>
2025-04-02 16:22:03 +0800

    test

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://127.0.0.1:5000',
-        changeOrigin: true,
-      },
-    },
   },
-  base: '/RAG/',
+  base: '/',
   build: {
     outDir: 'docs'
   }
