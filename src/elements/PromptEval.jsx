88bc2d828faf9b413a2b440bd9200c258096ab41
孙 浩翔 <221900209@smail.nju.edu.cn>
2025-04-02 20:21:03 +0800

    v0.0

--- src/elements/PromptEval.jsx
+++ src/elements/PromptEval.jsx
@@ -11,7 +11,7 @@ const PromptEval = () => {
   const handleSubmit = async () => {
     try {
       // 使用 axios 向后端发送 POST 请求
-      const response = await axios.post('http://2264b119.r10.cpolar.top/submit', {
+      const response = await axios.post('https://2264b119.r10.cpolar.top', {
         message: message
       });
 
